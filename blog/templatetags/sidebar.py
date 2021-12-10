354ead2a9ba07b5de06a30051abaa957fc543bfa
lzhecz <lzhecz@gmail.com>
2021-12-10 20:48:32 +0200

    Add tags cloud

--- blog/templatetags/sidebar.py
+++ blog/templatetags/sidebar.py
@@ -5,6 +5,12 @@ register = template.Library()
 
 
 @register.inclusion_tag('blog/popular.html')
-def get_popular_post(cnt=3):
+def get_popular_post(cnt=None):
     posts = Post.objects.order_by('-views')[:cnt]
     return {'posts': posts}
+
+
+@register.inclusion_tag('blog/tags_cloud.html')
+def get_tags_cloud(cnt=None):
+    tags = Tag.objects.all()[:cnt]
+    return {'tags': tags}
