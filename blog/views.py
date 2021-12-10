615c6b2ab6e7743ec76f09f9be091c555506631f
lzhecz <lzhecz@gmail.com>
2021-12-10 18:55:19 +0200

    Show post by tag

--- blog/views.py
+++ blog/views.py
@@ -32,7 +32,18 @@ class PostByCat(ListView):
 
 
 class PostByTag(ListView):
-    pass
+    template_name = 'blog/index.html'
+    context_object_name = 'posts'
+    paginate_by = 4
+    allow_empty = False
+
+    def get_queryset(self):
+        return Post.objects.filter(tags__slug = self.kwargs['slug'])
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Posts by tag: ' + str(Tag.objects.get(slug = self.kwargs['slug']))
+        return context
 
 
 class PostView(DetailView):
